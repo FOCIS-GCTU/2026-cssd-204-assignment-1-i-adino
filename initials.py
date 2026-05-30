f88c437ed1d42c6a0f0b00b8db89abf6e97fe372
i-adino <kwekuvan75@gmail.com>
2026-05-30 21:14:26 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+# Description: Prints the initials of my name.
+# Assignment: 1
+
+# Name: Adino Abeiku Vandyke
+# STUDENT ID:  2425402017
+# Email: <2425402017@gctu.edu.gh>
+# Grader: Augustus Buckman
+# 
+# On my honor, Adino Abeiku Vandyke, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
 
-
-
-main()
+    print()
+    print("...AAV")
+    print()
+    print("....AAAAAA.........AAAAAA...........VVVV......VVVV......")
+    print("...AAAAAAAA.......AAAAAAAA...........VVVV....VVVV.......")
+    print("..AAAA..AAAA.....AAAA..AAAA...........VVVV..VVVV........")
+    print("..AAAA..AAAA.....AAAA..AAAA............VVVVVVVV.........")
+    print("..AAAAAAAAAA.....AAAAAAAAAA............VVVVVVVV.........")
+    print("..AAAAAAAAAA.....AAAAAAAAAA.............VVVVVV..........")
+    print("..AAAA..AAAA.....AAAA..AAAA.............VVVVVV..........")
+    print("..AAAA..AAAA.....AAAA..AAAA..............VVVV...........")
+    print("..AAAA..AAAA..**..AAAA..AAAA..**.........VVVV..........*")
+    print("..AAAA..AAAA..**..AAAA..AAAA..**..........VV...........*")
+    print()
+    
